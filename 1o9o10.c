717932ac0608a477c75fbc9fe290ba2d545a4067
MaksymRopukha <61183857+MaksymRopukha@users.noreply.github.com>
2020-02-18 11:26:41 +0200

    Add files via upload
    Dz1
    1.9
    1.10
    Horaychuk Maksym

---
+++ 1o9o10.c
@@ -0,0 +1,44 @@
+ #include <stdio.h>
+ #include <math.h>
+
+
+ int main()
+ {
+     float x;                         // Zadacha 1.9
+     printf("input x = ");
+     scanf("%f", &x);
+     x = x*x;
+     x = x*x;
+     x = x*x;
+     printf("res = %f  \n \n Zadacha 1.10: \n", x);
+
+     printf("  x  |   1  |   2  |   3  |   4  |   5   \n");    // Zadacha 1.10
+     printf("---- + ---- + ---- + ---- + ---- + ----  \n");
+     printf("F(x) | %.2f | %.2f | %.2f | %.2f | %.2f \n", 
+     sqrt(1), sqrt(2), sqrt(3), sqrt(4), sqrt(5));
+
+ 
+     
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
